317420a2d3afb3e18eaeb95ab37b2853b57240a6
Aleksander Sumowski <github@sumowski.pl>
2024-04-14 00:33:19 +0200

    more chezmoi

---
+++ config/nvim/lua/plugins/user.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        keys = {{"<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = ""},
+        }
+    },
+    {"CarloWood/vim-plugin-AnsiEsc"},
+    {"m00qek/baleia.nvim", init = function() require("baleia").setup {} end},
+    {
+        "johmsalas/text-case.nvim",
+        init = function() require("textcase").setup {} end
+    },
+    {
+      'liouk/gitlinks.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        require('gitlinks').setup()
+      end,
+    }
+}

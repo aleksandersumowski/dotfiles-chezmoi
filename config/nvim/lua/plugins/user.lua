7105205f3f24ca207edeab68383dfb817cd3f47f
Aleksander Sumowski <github@sumowski.pl>
2024-04-14 21:37:18 +0200

    add chezmoi.nvim

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -1,20 +1,35 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        keys = {{"<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = ""},
-        }
-    },
-    {"CarloWood/vim-plugin-AnsiEsc"},
-    {"m00qek/baleia.nvim", init = function() require("baleia").setup {} end},
-    {
-        "johmsalas/text-case.nvim",
-        init = function() require("textcase").setup {} end
-    },
-    {
-      'liouk/gitlinks.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('gitlinks').setup()
-      end,
-    }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
+	},
+	{ "CarloWood/vim-plugin-AnsiEsc" },
+	{
+		"m00qek/baleia.nvim",
+		init = function()
+			require("baleia").setup({})
+		end,
+	},
+	{
+		"johmsalas/text-case.nvim",
+		init = function()
+			require("textcase").setup({})
+		end,
+	},
+	{
+		"liouk/gitlinks.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitlinks").setup()
+		end,
+	},
+	{
+		"xvzc/chezmoi.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("chezmoi").setup({
+				-- your configurations
+			})
+		end,
+	},
 }

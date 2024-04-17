2ddbe3efbc1d27a6b761bc059fc759e2b1a23a12
Aleksander Sumowski <github@sumowski.pl>
2024-04-17 15:38:40 +0200

    arch neutral, version independent fzf config

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,13 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.50.0/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/Cellar/fzf/0.50.0/bin"
+if [[ $OSTYPE == darwin* && $CPUTYPE == arm64 ]]; then
+    export FZF_PREFIX="/opt/homebrew/opt/fzf"
+else
+    export FZF_PREFIX="/usr/local/opt/fzf"
+fi
+
+if [[ ! "$PATH" == *$FZF_PREFIX/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$FZF_PREFIX/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.50.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$FZF_PREFIX/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/0.50.0/shell/key-bindings.zsh"
+source "$FZF_PREFIX/shell/key-bindings.zsh"

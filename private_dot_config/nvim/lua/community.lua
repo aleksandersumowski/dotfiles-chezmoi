d9485ef570a0180535937e47bed8e6ba6f634ae6
Aleksander Sumowski <github@sumowski.pl>
2024-04-17 16:53:41 +0200

    leap+flit -> flash

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -6,13 +6,14 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-    {import = "astrocommunity.pack.clojure"},
-    {import = "astrocommunity.pack.terraform"},
-    {import = "astrocommunity.project.nvim-spectre"},
-    {import = "astrocommunity.motion.mini-surround"},
-    {import = "astrocommunity.motion.leap-nvim"},
-    {import = "astrocommunity.motion.flit-nvim"}
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.clojure" },
+	{ import = "astrocommunity.pack.terraform" },
+	{ import = "astrocommunity.project.nvim-spectre" },
+	{ import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.motion.flash-nvim" },
+	-- {import = "astrocommunity.motion.leap-nvim"},
+	-- {import = "astrocommunity.motion.flit-nvim"}
+	-- import/override with your plugins folder
 }
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -1,20 +1,39 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        keys = {{"<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = ""},
-        }
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
+  },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
     },
-    {"CarloWood/vim-plugin-AnsiEsc"},
-    {"m00qek/baleia.nvim", init = function() require("baleia").setup {} end},
-    {
-        "johmsalas/text-case.nvim",
-        init = function() require("textcase").setup {} end
-    },
-    {
-      'liouk/gitlinks.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('gitlinks').setup()
-      end,
-    }
+  },
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("chezmoi").setup {
+        -- your configurations
+      }
+    end,
+  },
+  { "CarloWood/vim-plugin-AnsiEsc" },
+  {
+    "m00qek/baleia.nvim",
+    init = function() require("baleia").setup {} end,
+  },
+  {
+    "johmsalas/text-case.nvim",
+    init = function() require("textcase").setup {} end,
+  },
+  {
+    "liouk/gitlinks.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("gitlinks").setup() end,
+  },
 }

a86c266afc5182685bf8c65cfb592b37b20b1e2a
Aleksander Sumowski <github@sumowski.pl>
2024-08-01 11:25:52 +0200

    up to date work laptop

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -4,16 +4,16 @@
 
 ---@type LazySpec
 return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.clojure" },
-	{ import = "astrocommunity.pack.helm" },
-	{ import = "astrocommunity.pack.terraform" },
-	{ import = "astrocommunity.project.nvim-spectre" },
-	{ import = "astrocommunity.motion.mini-surround" },
-	{ import = "astrocommunity.motion.flash-nvim" },
-	{ import = "astrocommunity.indent.mini-indentscope" },
-	-- {import = "astrocommunity.motion.leap-nvim"},
-	-- {import = "astrocommunity.motion.flit-nvim"}
-	-- import/override with your plugins folder
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.clojure" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.search.nvim-spectre" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  -- {import = "astrocommunity.motion.leap-nvim"},
+  -- {import = "astrocommunity.motion.flit-nvim"}
+  -- import/override with your plugins folder
 }
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -1,55 +1,49 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
-	},
-	{
-		"folke/flash.nvim",
-		opts = {
-			modes = {
-				char = {
-					jump_labels = true,
-				},
-			},
-		},
-	},
-	{
-		"xvzc/chezmoi.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("chezmoi").setup({
-				-- your configurations
-			})
-		end,
-	},
-	{ "CarloWood/vim-plugin-AnsiEsc" },
-	{
-		"m00qek/baleia.nvim",
-		init = function()
-			require("baleia").setup({})
-		end,
-	},
-	{
-		"johmsalas/text-case.nvim",
-		init = function()
-			require("textcase").setup({})
-		end,
-	},
-	{
-		"liouk/gitlinks.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("gitlinks").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		event = "User AstroFile",
-		opts = {
-			options = {
-				border = "top",
-				try_as_border = true,
-			},
-		},
-	},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
+  },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  },
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("chezmoi").setup {
+        -- your configurations
+      }
+    end,
+  },
+  { "CarloWood/vim-plugin-AnsiEsc" },
+  {
+    "m00qek/baleia.nvim",
+    init = function() require("baleia").setup {} end,
+  },
+  {
+    "johmsalas/text-case.nvim",
+    init = function() require("textcase").setup {} end,
+  },
+  {
+    "liouk/gitlinks.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("gitlinks").setup() end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    event = "User AstroFile",
+    opts = {
+      options = {
+        border = "top",
+        try_as_border = true,
+      },
+    },
+  },
 }

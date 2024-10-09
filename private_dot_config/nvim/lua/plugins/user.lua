a4181b04480d9a8f6eddbd02cacecfe9bbd175cc
Aleksander Sumowski <github@sumowski.pl>
2024-10-09 12:54:24 +0200

    classify user plugins based on usage status
    whether it's accepted changbe
    or just trying out

--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -1,56 +1,53 @@
 return {
-	{ "mrjones2014/smart-splits.nvim" },
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
-	},
-	{
-		"folke/flash.nvim",
-		opts = {
-			modes = {
-				char = {
-					jump_labels = true,
-				},
-			},
-		},
-	},
-	{
-		"xvzc/chezmoi.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("chezmoi").setup({
-				-- your configurations
-			})
-		end,
-	},
-	{ "CarloWood/vim-plugin-AnsiEsc" },
-	{
-		"m00qek/baleia.nvim",
-		init = function()
-			require("baleia").setup({})
-		end,
-	},
-	{
-		"johmsalas/text-case.nvim",
-		init = function()
-			require("textcase").setup({})
-		end,
-	},
-	{
-		"liouk/gitlinks.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("gitlinks").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		event = "User AstroFile",
-		opts = {
-			options = {
-				border = "top",
-				try_as_border = true,
-			},
-		},
-	},
+  -- testing!
+  { "sindrets/diffview.nvim" },
+  { "cenk1cenk2/schema-companion.nvim" },
+  {
+    "cenk1cenk2/jq.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "grapp-dev/nui-components.nvim",
+    },
+  },
+  {
+    "ksaito422/remote-line.nvim",
+    config = function() require("remote-line").setup {} end,
+  },
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("chezmoi").setup {} end,
+  },
+  {
+    "johmsalas/text-case.nvim",
+    init = function() require("textcase").setup {} end,
+  },
+
+  -- config overrides
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = { { "<leader>e", "<cmd>Neotree focus reveal_force_cwd<cr>", desc = "" } },
+  },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  },
+  -- keep
+  {
+    "echasnovski/mini.indentscope",
+    event = "User AstroFile",
+    opts = {
+      options = {
+        border = "top",
+        try_as_border = true,
+      },
+    },
+  },
 }

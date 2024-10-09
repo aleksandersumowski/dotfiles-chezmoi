929eb2ac45e3a3a07d919eb09f9cf00ce4b6809a
Aleksander Sumowski <github@sumowski.pl>
2024-10-09 13:03:10 +0200

    setup tooling for correct version of kube

--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -1,7 +1,28 @@
 return {
   -- testing!
   { "sindrets/diffview.nvim" },
-  { "cenk1cenk2/schema-companion.nvim" },
+  {
+    "cenk1cenk2/schema-companion.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("schema-companion").setup {
+        -- if you have telescope you can register the extension
+        enable_telescope = true,
+        matchers = {
+          require("schema-companion.matchers.kubernetes").setup { version = "master" },
+        },
+        schemas = {
+          {
+            name = "Kubernetes v1.29",
+            uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.7-standalone-strict/all.json",
+          },
+        },
+      }
+    end,
+  },
   {
     "cenk1cenk2/jq.nvim",
     dependencies = {
@@ -12,7 +33,7 @@ return {
   },
   {
     "ksaito422/remote-line.nvim",
-    config = function() require("remote-line").setup {} end,
+    config = function() require("remote-line").setup() end,
   },
   {
     "xvzc/chezmoi.nvim",

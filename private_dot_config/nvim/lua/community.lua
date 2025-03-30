7be7859d0cb140ba66fca4b5ce228b877f4f593f
Aleksander Sumowski <github@sumowski.pl>
2025-03-30 14:19:48 +0200

    AstroNvim v5 for neovim 0.11

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -16,8 +16,6 @@ return {
   { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-  { import = "astrocommunity/code-runner/sniprun" },
-  -- {import = "astrocommunity.motion.leap-nvim"},
-  -- {import = "astrocommunity.motion.flit-nvim"}
+  -- { import = "astrocommunity/code-runner/sniprun" },
   -- import/override with your plugins folder
 }
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -1,3 +1,8 @@
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
+-- Here are some examples:
+
+---@type LazySpec
 return {
   -- testing!
   { "sindrets/diffview.nvim" },
